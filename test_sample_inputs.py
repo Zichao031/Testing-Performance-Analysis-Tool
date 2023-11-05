8c6d64b9e7356b44fd72186379635ea4828612e7
Zichao001 <zxiao031@ucr.edu>
2023-11-05 11:45:44 -0800

    Add visualization of results(inputs/execution time)

--- test_sample_inputs.py
+++ test_sample_inputs.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 import time
-
+import matplotlib.pyplot as plt
 
 # Compile the C program
 subprocess.run(['gcc', '-o', 'track_time', 'track_time.c'], check=True)
@@ -32,3 +32,39 @@ os.remove('track_time')
 # Print the execution times
 for args, time_taken in execution_times.items():
     print(f'Input: {args} - Execution Time: {time_taken} seconds')
+
+
+# Extract the input arguments as labels and the execution times as values
+input_labels = list(execution_times.keys())
+execution_time_values = list(execution_times.values())
+
+# Plot the execution times
+plt.figure(figsize=(10, 6))
+plt.bar(input_labels, execution_time_values, color='skyblue')
+
+plt.title('Execution Time Distribution for Different Inputs')
+plt.xlabel('Input')
+plt.ylabel('Execution Time (seconds)')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Display the plot
+plt.show()
+
+
+
+
+# # Normalize the histogram, so the sum of the bin heights is equal to 1 to represent probability
+# plt.figure(figsize=(10, 6))
+# n, bins, patches = plt.hist(execution_time_values, bins=10, color='skyblue', alpha=0.7, density=True)
+
+# plt.title('Probability Distribution of Execution Times')
+# plt.xlabel('Execution Time (seconds)')
+# plt.ylabel('Probability')
+
+# # Annotate the bin heights (probabilities) on the histogram
+# for i in range(len(patches)):
+#     plt.text(patches[i].xy[0], patches[i].get_height(), f'{patches[i].get_height():.2f}', ha='center', va='bottom')
+
+# plt.tight_layout()  # Adjust layout to fit everything nicely
+# plt.show()
